5584ef4acf8b6858e90d6c09a7b915d9b0e10670
Kohle Christner <kohlec@fireflyint.com>
2021-03-08 20:20:50 -0500

    Trying this again. Having some issues with settings up the initial commit for the project but hopefully this is it!

---
+++ Pokemon.cls.js
@@ -0,0 +1,7 @@
+export default class Pokemon {
+  constructor(name, type, moves) {
+    this.name = name,
+    this.type = type,
+    this.moves = moves
+  }
+}
---
+++ Pokemon.obj.js
@@ -0,0 +1,11 @@
+import Pokemon from "/Pokemon.cls.js";
+
+const Bulbasaur = new Pokemon('Bulbasaur', ['Grass'], ['Tackle', 'Vine Whip']);
+const Ivysaur = new Pokemon('Ivysaur', ['Grass', 'Poison'], ['Tackle', 'Razor Leaf', 'Leech Seed', ])
+const Venasaur = new Pokemon('Venasaur', ['Grass/Poison'], ['Solar Beam', 'Razor Leaf', 'Synthesis', 'Poison Powder'])
+const Squirtle = new Pokemon('Squirtle', ['Water'], ['Pound', 'Bubble']);
+const Wartortle = new Pokemon('Wartortle', ['Water'], ['Pound', 'Water Gun', 'Bubblebeam'])
+const Blastoise = new Pokemon('Blastoise', ['Water'], ['Surf', 'Skull Bash', 'Hydro Pump', 'Mega Punch']);
+const Charmander = new Pokemon('Charmander', ['Fire'], ['Scratch', 'Ember']);
+const Charmeleon = new Pokemon('Charmeleon', ['Fire'], ['Slash', 'Ember', 'Fire Punch']);
+const Charizard = new Pokemon('Charizard', ['Fire'], ['Dragon Claw', 'Fly', 'Fire Blast', 'Flamethrower']);
