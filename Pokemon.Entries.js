7781480f0393f0f077c15f2a772fedcc95b0f221
Kohle Christner <kohlec@fireflyint.com>
2021-04-27 21:26:28 -0400

    Reformatted html and css. Also added png images to project

--- Pokemon.Entries.js
+++ Pokemon.Entries.js
@@ -4,7 +4,6 @@ let entry;
 
 // Create a for loop that iterates throught the pokemon-entry class
 let pokemonEntry = document.querySelectorAll(".pokemon-entry");
-console.log(pokemonEntry)
 
 for (let i = 0; i < pokemonEntry.length; i++) {
     pokemonEntry[i].textContent = pokemonArray[i].name
