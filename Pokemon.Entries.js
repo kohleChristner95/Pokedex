a654aed87ac2293348fdd197420fccad5a7eeab8
Kohle Christner <kohlec@fireflyint.com>
2021-04-28 21:01:28 -0400

    Added more styling and additional divs. Started adding click event to pokemon-entry elements

--- Pokemon.Entries.js
+++ Pokemon.Entries.js
@@ -1,11 +1,15 @@
 import pokemonArray from './Pokemon.obj.js'
 
 let entry;
+const text = document.querySelector('.text')
+const picture = document.querySelector('.picture');
+const pictureBackdrop = document.querySelector('.smaller-dark-square');
+const pokemonEntry = document.querySelectorAll(".pokemon-entry");
 
-// Create a for loop that iterates throught the pokemon-entry class
-let pokemonEntry = document.querySelectorAll(".pokemon-entry");
-
+// Create a for loop that iterates through the pokemon-entry class
 for (let i = 0; i < pokemonEntry.length; i++) {
-    pokemonEntry[i].textContent = pokemonArray[i].name
-    console.log(pokemonArray[i].name)
+    pokemonEntry[i].textContent = [i + 1] + " " + pokemonArray[i].name
+    pokemonEntry[i].addEventListener('click', function() {
+        text.textContent = pokemonArray[i].name;
+    })
 }
