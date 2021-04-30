13f6be11e2860765b3cf4548aa4e55f28d53f26c
Kohle Christner <kohlec@fireflyint.com>
2021-04-29 20:31:32 -0400

    Added picture id to project. Started adding picture feature as well. cleaned things up a little as well.

--- Pokemon.Entries.js
+++ Pokemon.Entries.js
@@ -1,15 +1,28 @@
 import pokemonArray from './Pokemon.obj.js'
 
-let entry;
 const text = document.querySelector('.text')
-const picture = document.querySelector('.picture');
+const picture = document.querySelector('#picture');
 const pictureBackdrop = document.querySelector('.smaller-dark-square');
 const pokemonEntry = document.querySelectorAll(".pokemon-entry");
+const clear = document.getElementById('clear');
+
+clear.style.cursor = "pointer";
 
 // Create a for loop that iterates through the pokemon-entry class
 for (let i = 0; i < pokemonEntry.length; i++) {
+    pokemonEntry[i].style.cursor = "pointer";
     pokemonEntry[i].textContent = [i + 1] + " " + pokemonArray[i].name
     pokemonEntry[i].addEventListener('click', function() {
+        let img = document.createElement("img");
+        let src = document.getElementById("picture");
+        img.src = "Bulbasaur.png";
+        src.appendChild(img);
+
         text.textContent = pokemonArray[i].name;
     })
 }
+
+clear.addEventListener('click', clearText => {
+    text.textContent = '';
+    picture.textContent = '';
+})
