1688146a5895186706bfc5a7691cadb279373d52
Kohle Christner <kohlec@fireflyint.com>
2021-06-07 19:57:32 -0400

    Resized images and expanded on class

--- Pokemon.cls.js
+++ Pokemon.cls.js
@@ -1,7 +1,8 @@
 export default class Pokemon {
-  constructor(name, type, moves) {
+  constructor(name, type, moves, pic) {
     this.name = name,
     this.type = type,
-    this.moves = moves
+    this.moves = moves,
+    this.pic = pic
   }
 }
--- Pokemon.obj.js
+++ Pokemon.obj.js
@@ -2,16 +2,15 @@ import Pokemon from "/Pokemon.cls.js";
 
 const pokemonArray = [];
 
-const Bulbasaur = new Pokemon('Bulbasaur', ['Grass'], ['Tackle', 'Vine Whip']);
-const Ivysaur = new Pokemon('Ivysaur', ['Grass', 'Poison'], ['Tackle', 'Razor Leaf', 'Leech Seed', ])
-const Venasaur = new Pokemon('Venasaur', ['Grass/Poison'], ['Solar Beam', 'Razor Leaf', 'Synthesis', 'Poison Powder'])
-const Squirtle = new Pokemon('Squirtle', ['Water'], ['Pound', 'Bubble']);
-const Wartortle = new Pokemon('Wartortle', ['Water'], ['Pound', 'Water Gun', 'Bubblebeam'])
-const Blastoise = new Pokemon('Blastoise', ['Water'], ['Surf', 'Skull Bash', 'Hydro Pump', 'Mega Punch']);
-const Charmander = new Pokemon('Charmander', ['Fire'], ['Scratch', 'Ember']);
-const Charmeleon = new Pokemon('Charmeleon', ['Fire'], ['Slash', 'Ember', 'Fire Punch']);
-const Charizard = new Pokemon('Charizard', ['Fire'], ['Dragon Claw', 'Fly', 'Fire Blast', 'Flamethrower']);
+const Bulbasaur = new Pokemon('Bulbasaur', ['Grass'], ['Tackle', 'Vine Whip'], "Bulbasaur.png");
+const Ivysaur = new Pokemon('Ivysaur', ['Grass', 'Poison'], ['Tackle', 'Razor Leaf', 'Leech Seed'], "Ivysaur.png")
+const Venasaur = new Pokemon('Venasaur', ['Grass/Poison'], ['Solar Beam', 'Razor Leaf', 'Synthesis', 'Poison Powder'], "Venasaur.png")
+const Squirtle = new Pokemon('Squirtle', ['Water'], ['Pound', 'Bubble'], "Squirtle.png");
+const Wartortle = new Pokemon('Wartortle', ['Water'], ['Pound', 'Water Gun', 'Bubblebeam'], "Wartortle.png")
+const Blastoise = new Pokemon('Blastoise', ['Water'], ['Surf', 'Skull Bash', 'Hydro Pump', 'Mega Punch'], "Blastoise.png");
+const Charmander = new Pokemon('Charmander', ['Fire'], ['Scratch', 'Ember'], "Charmander.png");
+const Charmeleon = new Pokemon('Charmeleon', ['Fire'], ['Slash', 'Ember', 'Fire Punch'], "Charmeleon.png");
+const Charizard = new Pokemon('Charizard', ['Fire'], ['Dragon Claw', 'Fly', 'Fire Blast', 'Flamethrower'], "Charizard.png");
 
-pokemonArray.push(Bulbasaur, Ivysaur, Venasaur, Squirtle, Wartortle, Blastoise, Charmander, Charmeleon, Charizard)
-
+pokemonArray.push(Bulbasaur, Ivysaur, Venasaur, Squirtle, Wartortle, Blastoise, Charmander, Charmeleon, Charizard);
 export default pokemonArray;
