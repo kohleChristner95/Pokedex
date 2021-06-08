ffa789d60122acaf4077683647b741eec33d7618
Kohle Christner <kohlec@fireflyint.com>
2021-06-07 20:26:22 -0400

    Added functionality to make entries clear when a new one is pressed. Cleaned up more code.

--- Pokemon.Entries.js
+++ Pokemon.Entries.js
@@ -7,22 +7,29 @@ const pokemonEntry = document.querySelectorAll(".pokemon-entry");
 const clear = document.getElementById('clear');
 
 clear.style.cursor = "pointer";
+const clearText = function () {
+    text.textContent = '';
+    picture.textContent = '';
+}
 
 // Create a for loop that iterates through the pokemon-entry class
 for (let i = 0; i < pokemonEntry.length; i++) {
     pokemonEntry[i].style.cursor = "pointer";
     pokemonEntry[i].textContent = [i + 1] + " " + pokemonArray[i].name
     pokemonEntry[i].addEventListener('click', function() {
-        let img = document.createElement("img");
-        let src = document.getElementById("picture");
-        img.src = "Bulbasaur.png";
-        src.appendChild(img);
-
-        text.textContent = pokemonArray[i].name;
+        let initializePictureText = function () {
+            let img = document.createElement("img");
+            let src = document.getElementById("picture");
+            img.src = pokemonArray[i].pic;
+            src.appendChild(img);
+            img.src = pokemonArray[i].pic;
+            text.textContent = pokemonArray[i].name;
+        }
+        if (picture.textContent !== 'undefined' && text.textContent !== 'undefined') {
+            clearText();
+            initializePictureText();
+        }
     })
 }
 
-clear.addEventListener('click', clearText => {
-    text.textContent = '';
-    picture.textContent = '';
-})
+clear.addEventListener('click', clearText)
