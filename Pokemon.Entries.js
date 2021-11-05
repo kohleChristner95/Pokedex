7abfead606f6f24f7dc097723570fbef89508291
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2021-11-04 20:45:22 -0400

    Cleaning up for-loop. Also, adding fetch for pokemon img

--- Pokemon.Entries.js
+++ Pokemon.Entries.js
@@ -15,14 +15,13 @@ const clearText = function () {
 // Create a for loop that iterates through the pokemon-entry class
 for (let i = 0; i < pokemonEntry.length; i++) {
     pokemonEntry[i].style.cursor = "pointer";
-    pokemonEntry[i].textContent = [i + 1] + " " + pokemonArray[i].name
+    pokemonEntry[i].textContent = [i + 1] + " " + pokemonArray[i].name // Lists the number and the pokemon name
     pokemonEntry[i].addEventListener('click', function() {
         let initializePictureText = function () {
             let img = document.createElement("img");
             let src = document.getElementById("picture");
             img.src = pokemonArray[i].pic;
             src.appendChild(img);
-            img.src = pokemonArray[i].pic;
             text.textContent = pokemonArray[i].name;
         }
         if (picture.textContent !== 'undefined' && text.textContent !== 'undefined') {
@@ -33,3 +32,19 @@ for (let i = 0; i < pokemonEntry.length; i++) {
 }
 
 clear.addEventListener('click', clearText)
+
+const getPokemonimg = function(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => {
+        console.log(response)
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data.sprites.versions['generation-iv']['heartgold-soulsilver'].front_default)
+    })
+}
+getPokemonimg('charmander');
+getPokemonimg('bulbasaur');
+getPokemonimg('gible');
+getPokemonimg('pikachu')
+// console.log(charmander)
