091dbd053e4761a0c8e7e6825e897e0e5a1f01b8
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2021-11-05 22:06:37 -0400

    en route to adding dynamic divs

--- Pokemon.Entries.js
+++ Pokemon.Entries.js
@@ -3,8 +3,16 @@ import pokemonArray from './Pokemon.obj.js'
 const text = document.querySelector('.text')
 const picture = document.querySelector('#picture');
 const pictureBackdrop = document.querySelector('.smaller-dark-square');
-const pokemonEntry = document.querySelectorAll(".pokemon-entry");
+// const pokemonEntry = document.querySelectorAll(".pokemon-entry");
+const pokemonEntryDiv = document.createElement('div');
+pokemonEntryDiv.className = "pokemon-entry";
+const innerScroll = document.querySelector('.Inner');
+innerScroll.appendChild(pokemonEntryDiv)
+console.log(innerScroll)
 const clear = document.getElementById('clear');
+// console.log(innerScroll, pokemonEntry)
+
+// console.log(document.body)
 
 clear.style.cursor = "pointer";
 const clearText = function () {
@@ -13,38 +21,22 @@ const clearText = function () {
 }
 
 // Create a for loop that iterates through the pokemon-entry class
-for (let i = 0; i < pokemonEntry.length; i++) {
-    pokemonEntry[i].style.cursor = "pointer";
-    pokemonEntry[i].textContent = [i + 1] + " " + pokemonArray[i].name // Lists the number and the pokemon name
-    pokemonEntry[i].addEventListener('click', function() {
-        let initializePictureText = function () {
-            let img = document.createElement("img");
-            let src = document.getElementById("picture");
-            img.src = pokemonArray[i].pic;
-            src.appendChild(img);
-            text.textContent = pokemonArray[i].name;
-        }
-        if (picture.textContent !== 'undefined' && text.textContent !== 'undefined') {
-            clearText();
-            initializePictureText();
-        }
-    })
-}
+// for (let i = 0; i < pokemonEntry.length; i++) {
+//     pokemonEntry[i].style.cursor = "pointer";
+//     pokemonEntry[i].textContent = [i + 1] + " " + pokemonArray[i].name // Lists the number and the pokemon name
+//     pokemonEntry[i].addEventListener('click', function() {
+//         let initializePictureText = function () {
+//             let img = document.createElement("img");
+//             let src = document.getElementById("picture");
+//             img.src = pokemonArray[i].pic;
+//             src.appendChild(img);
+//             text.textContent = pokemonArray[i].name;
+//         }
+//         if (picture.textContent !== 'undefined' && text.textContent !== 'undefined') {
+//             clearText();
+//             initializePictureText();
+//         }
+//     })
+// }
 
 clear.addEventListener('click', clearText)
-
-const getPokemonimg = function(pokemon) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then((response) => {
-        console.log(response)
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data.sprites.versions['generation-iv']['heartgold-soulsilver'].front_default)
-    })
-}
-getPokemonimg('charmander');
-getPokemonimg('bulbasaur');
-getPokemonimg('gible');
-getPokemonimg('pikachu')
-// console.log(charmander)
--- Pokemon.obj.js
+++ Pokemon.obj.js
@@ -1,16 +1,41 @@
-import Pokemon from "/Pokemon.cls.js";
+// import Pokemon from "/Pokemon.cls.js";
 
-const pokemonArray = [];
+// const pokemonArray = [];
 
-const Bulbasaur = new Pokemon('Bulbasaur', ['Grass'], ['Tackle', 'Vine Whip'], "Bulbasaur.png");
-const Ivysaur = new Pokemon('Ivysaur', ['Grass', 'Poison'], ['Tackle', 'Razor Leaf', 'Leech Seed'], "Ivysaur.png")
-const Venasaur = new Pokemon('Venasaur', ['Grass/Poison'], ['Solar Beam', 'Razor Leaf', 'Synthesis', 'Poison Powder'], "Venasaur.png")
-const Squirtle = new Pokemon('Squirtle', ['Water'], ['Pound', 'Bubble'], "Squirtle.png");
-const Wartortle = new Pokemon('Wartortle', ['Water'], ['Pound', 'Water Gun', 'Bubblebeam'], "Wartortle.png")
-const Blastoise = new Pokemon('Blastoise', ['Water'], ['Surf', 'Skull Bash', 'Hydro Pump', 'Mega Punch'], "Blastoise.png");
-const Charmander = new Pokemon('Charmander', ['Fire'], ['Scratch', 'Ember'], "Charmander.png");
-const Charmeleon = new Pokemon('Charmeleon', ['Fire'], ['Slash', 'Ember', 'Fire Punch'], "Charmeleon.png");
-const Charizard = new Pokemon('Charizard', ['Fire'], ['Dragon Claw', 'Fly', 'Fire Blast', 'Flamethrower'], "Charizard.png");
+// const Bulbasaur = new Pokemon('Bulbasaur', ['Grass'], ['Tackle', 'Vine Whip'], "Bulbasaur.png");
+// const Ivysaur = new Pokemon('Ivysaur', ['Grass', 'Poison'], ['Tackle', 'Razor Leaf', 'Leech Seed'], "Ivysaur.png")
+// const Venasaur = new Pokemon('Venasaur', ['Grass/Poison'], ['Solar Beam', 'Razor Leaf', 'Synthesis', 'Poison Powder'], "Venasaur.png")
+// const Squirtle = new Pokemon('Squirtle', ['Water'], ['Pound', 'Bubble'], "Squirtle.png");
+// const Wartortle = new Pokemon('Wartortle', ['Water'], ['Pound', 'Water Gun', 'Bubblebeam'], "Wartortle.png")
+// const Blastoise = new Pokemon('Blastoise', ['Water'], ['Surf', 'Skull Bash', 'Hydro Pump', 'Mega Punch'], "Blastoise.png");
+// const Charmander = new Pokemon('Charmander', ['Fire'], ['Scratch', 'Ember'], "Charmander.png");
+// const Charmeleon = new Pokemon('Charmeleon', ['Fire'], ['Slash', 'Ember', 'Fire Punch'], "Charmeleon.png");
+// const Charizard = new Pokemon('Charizard', ['Fire'], ['Dragon Claw', 'Fly', 'Fire Blast', 'Flamethrower'], "Charizard.png");
 
-pokemonArray.push(Bulbasaur, Ivysaur, Venasaur, Squirtle, Wartortle, Blastoise, Charmander, Charmeleon, Charizard);
-export default pokemonArray;
+// pokemonArray.push(Bulbasaur, Ivysaur, Venasaur, Squirtle, Wartortle, Blastoise, Charmander, Charmeleon, Charizard);
+// export default pokemonArray;
+
+const getPokemonImg = function(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => {
+        // console.log(response)
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data.sprites.versions['generation-iv']['heartgold-soulsilver'].front_default)
+        // console.log(data)
+    })
+}
+// getPokemonImg('bulbasaur');
+
+var kantoRegion = [];
+for (let i = 1; i <= 151; i++) {
+    kantoRegion.push(i);
+    // console.log(i)
+    // console.log(kantoRegion)
+}
+for (let i = 0; i <= kantoRegion.length; i++) {
+    // getPokemonImg(i)
+}
+
+export default kantoRegion;
